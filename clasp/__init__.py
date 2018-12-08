0b43fb5b6c2bf8edd7b09e86d3b5f0109032df1e
stephen wasilewski <stephen@coolshadow.com>
2018-12-07 16:45:44 -0800

    make ready to tag

--- clasp/__init__.py
+++ clasp/__init__.py
@@ -4,5 +4,5 @@
 
 __author__ = """Stephen Wasilewski"""
 __email__ = 'stephanwaz@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.0.0'
 __all__ = ['script_tools', 'click_ext', 'sphinx_click_ext']
