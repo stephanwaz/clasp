13a24f9888016230ffab3442c981aa342660bc3c
stephen wasilewski <stephen@coolshadow.com>
2018-12-10 17:27:40 -0800

    make extended click importable from clasp.click

--- clasp/__init__.py
+++ clasp/__init__.py
@@ -6,3 +6,8 @@ __author__ = """Stephen Wasilewski"""
 __email__ = 'stephanwaz@gmail.com'
 __version__ = '0.0.1'
 __all__ = ['script_tools', 'click_ext', 'sphinx_click_ext']
+
+import click
+import clasp.click_ext
+
+click = clasp.click_ext.click_ext(click)
