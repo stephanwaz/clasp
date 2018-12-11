ec9cb24f3fb801ddcd36d9451092015beaf95ad9
stephen wasilewski <stephen@coolshadow.com>
2018-12-10 17:29:19 -0800

    Bump version: 0.0.1 → 0.1.0

--- HISTORY.rst
+++ HISTORY.rst
@@ -2,6 +2,11 @@
 History
 =======
 
+
+0.1.0
+-----
+first stable release
+
 0.0.1
 -----
 Initial setup of the directory
--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://bitbucket.org/loisosubbelohde/clasp.git',
-    version='0.0.1',
+    version='0.1.0',
     zip_safe=True,
 )
