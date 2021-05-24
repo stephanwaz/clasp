161be4d051d026f24704c20652c83c87010687e0
Stephen Wasilewski <stephanwaz@gmail.com>
2021-05-24 10:27:04 +0200

    Bump version: 1.1.4 → 1.1.5

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     tests_require=test_requirements,
     url='https://bitbucket.org/stephenwasilewski/clasp',
     project_urls= {'documentation': 'https://clasp.readthedocs.io/'},
-    version='1.1.4',
+    version='1.1.5',
     zip_safe=True,
 )
